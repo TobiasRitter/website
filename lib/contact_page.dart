67380fb6ac7c2338050d48e8230d96cc3a34087d
TobiasRitter <tobias.ritter98@gmail.com>
2021-03-10 18:38:44 +0100

    remove unnecessary scaffold

--- lib/contact_page.dart
+++ lib/contact_page.dart
@@ -12,29 +12,27 @@ class ContactPage extends StatelessWidget {
       dark: true,
       child: Padding(
         padding: const EdgeInsets.all(64),
-        child: Scaffold(
-          body: Column(
-            children: [
-              Row(
-                children: [
-                  Icon(
-                    Icons.alternate_email,
-                    size: 96,
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Icon(
+                  Icons.alternate_email,
+                  size: 96,
+                ),
+                Container(
+                  width: 32,
+                ),
+                Text(
+                  "Contact",
+                  style: TextStyle(
+                    fontSize: 96,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Container(
-                    width: 32,
-                  ),
-                  Text(
-                    "Contact",
-                    style: TextStyle(
-                      fontSize: 96,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
