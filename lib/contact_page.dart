caeecd2cf782d38f5d4e6859c8803169161f4b4c
TobiasRitter <tobias.ritter98@gmail.com>
2021-03-12 19:09:10 +0100

    work on contact page

--- lib/contact_page.dart
+++ lib/contact_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:website/main.dart';
 import 'package:website/page.dart' as p;
 
 class ContactPage extends StatelessWidget {
@@ -8,6 +9,7 @@ class ContactPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
     return p.Page(
       dark: true,
       child: Padding(
@@ -31,6 +33,100 @@ class ContactPage extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+            Container(
+              height: 64,
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Wrap(
+                    children: [
+                      Container(
+                        width: screenWidth > 800
+                            ? (screenWidth - 128) / 2
+                            : screenWidth - 128,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Let's get in contact:",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 48,
+                              ),
+                            ),
+                            Container(
+                              height: 64,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                FloatingActionButton.extended(
+                                  onPressed: null,
+                                  label: Text(
+                                    "LinkedIn",
+                                  ),
+                                ),
+                                Container(
+                                  width: 32,
+                                ),
+                                FloatingActionButton.extended(
+                                  onPressed: null,
+                                  label: Text("XING"),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              height: 64,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: screenWidth > 800
+                            ? (screenWidth - 128) / 2
+                            : screenWidth - 128,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Or take a look at my code:",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 48,
+                              ),
+                            ),
+                            Container(
+                              height: 64,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                FloatingActionButton.extended(
+                                  onPressed: null,
+                                  label: Text("GitHub"),
+                                ),
+                                Container(
+                                  width: 32,
+                                ),
+                                FloatingActionButton.extended(
+                                  onPressed: null,
+                                  label: Text("Docker Hub"),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              height: 64,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
