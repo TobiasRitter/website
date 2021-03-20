503ecc9d9c83d09b547cee063f584b32a19e939e
TobiasRitter <tobias.ritter98@gmail.com>
2021-03-20 19:03:49 +0100

    improve resume margins

--- lib/resume_section.dart
+++ lib/resume_section.dart
@@ -27,84 +27,87 @@ class _ResumeSectionState extends State<ResumeSection> {
   Widget build(BuildContext context) {
     var marginSize = getMarginSize(context);
     var screenWidth = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: EdgeInsets.only(
-          bottom: screenWidth > SWIDTH ? marginSize : marginSize * 2),
-      child: Wrap(
-        spacing: marginSize / 2,
-        runSpacing: marginSize,
-        children: [
-          Container(
-            width: screenWidth > SWIDTH ? 300 : null,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  widget.date,
-                  style: textStyle.copyWith(
-                    color: Theme.of(context).textTheme.bodyText1!.color,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 8),
-                  child: Text(
-                    widget.title,
-                    style: h3Style.copyWith(
+    return Container(
+      width: screenWidth > SWIDTH ? screenWidth / 2 : null,
+      child: Padding(
+        padding: EdgeInsets.only(
+            bottom: screenWidth > SWIDTH ? marginSize : marginSize * 2),
+        child: Wrap(
+          spacing: marginSize / 2,
+          runSpacing: marginSize,
+          children: [
+            Container(
+              width: screenWidth > SWIDTH ? 300 : null,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.date,
+                    style: textStyle.copyWith(
                       color: Theme.of(context).textTheme.bodyText1!.color,
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 32),
-                  child: Chip(
-                    label: Text(
-                      widget.location,
-                      style: textStyle.copyWith(
+                  Padding(
+                    padding: EdgeInsets.only(top: 8),
+                    child: Text(
+                      widget.title,
+                      style: h3Style.copyWith(
                         color: Theme.of(context).textTheme.bodyText1!.color,
                       ),
                     ),
-                    avatar: Icon(
-                      Icons.location_pin,
-                      size: textStyle.fontSize,
-                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 8),
-                  child: Chip(
-                    label: Text(
-                      widget.institution,
-                      textAlign: TextAlign.end,
-                      style: textStyle.copyWith(
-                        color: Theme.of(context).textTheme.bodyText1!.color,
+                  Padding(
+                    padding: EdgeInsets.only(top: 32),
+                    child: Chip(
+                      label: Text(
+                        widget.location,
+                        style: textStyle.copyWith(
+                          color: Theme.of(context).textTheme.bodyText1!.color,
+                        ),
+                      ),
+                      avatar: Icon(
+                        Icons.location_pin,
+                        size: textStyle.fontSize,
                       ),
                     ),
-                    avatar: Icon(
-                      Icons.house,
-                      size: textStyle.fontSize,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 8),
+                    child: Chip(
+                      label: Text(
+                        widget.institution,
+                        textAlign: TextAlign.end,
+                        style: textStyle.copyWith(
+                          color: Theme.of(context).textTheme.bodyText1!.color,
+                        ),
+                      ),
+                      avatar: Icon(
+                        Icons.house,
+                        size: textStyle.fontSize,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Container(
-            width: screenWidth > SWIDTH
-                ? (screenWidth - 7 * marginSize) / 2 - 300 - marginSize / 2
-                : null,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                AutoSizeText(
-                  widget.description,
-                  style: textStyle.copyWith(
-                    color: Theme.of(context).textTheme.bodyText1!.color,
+            Container(
+              width: screenWidth > SWIDTH
+                  ? screenWidth / 2 - 300 - marginSize / 2
+                  : null,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  AutoSizeText(
+                    widget.description,
+                    style: textStyle.copyWith(
+                      color: Theme.of(context).textTheme.bodyText1!.color,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
