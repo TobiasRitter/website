2590b060f113b9be79a08e8234268c293aa086f6
TobiasRitter <tobias.ritter98@gmail.com>
2021-03-21 11:05:05 +0100

    add more vertical padding between sections

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -16,37 +16,40 @@ class AboutPage extends StatelessWidget {
     var screenWidth = MediaQuery.of(context).size.width;
     return p.Page(
       coverScreenHeight: true,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            constraints: BoxConstraints(
-                maxWidth: screenWidth > MWIDTH ? MWIDTH : screenWidth),
-            child: Padding(
-              padding: EdgeInsets.all(marginSize),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Header(
-                    text: "About",
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          "My name is Tobias Ritter and I am a Computer Science student currently working on my master's degree. I am highly interested in Data Analytics and Machine learning and love to develop Flutter applications like this website in my free time.",
-                          style: textStyle,
-                          softWrap: true,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: marginSize * 3),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              constraints: BoxConstraints(
+                  maxWidth: screenWidth > MWIDTH ? MWIDTH : screenWidth),
+              child: Padding(
+                padding: EdgeInsets.all(marginSize),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Header(
+                      text: "About",
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            "My name is Tobias Ritter and I am a Computer Science student currently working on my master's degree. I am highly interested in Data Analytics and Machine learning and love to develop Flutter applications like this website in my free time.",
+                            style: textStyle,
+                            softWrap: true,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/resume_page.dart
+++ lib/resume_page.dart
@@ -16,72 +16,75 @@ class ResumePage extends StatelessWidget {
     return p.Page(
       dark: true,
       coverScreenHeight: true,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            constraints: BoxConstraints(
-                maxWidth: screenWidth > MWIDTH ? MWIDTH : screenWidth),
-            child: Padding(
-              padding: EdgeInsets.all(marginSize),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Header(
-                    text: "Resume",
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: marginSize),
-                    child: Text(
-                      "Education",
-                      style: h2Style,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: marginSize * 3),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              constraints: BoxConstraints(
+                  maxWidth: screenWidth > MWIDTH ? MWIDTH : screenWidth),
+              child: Padding(
+                padding: EdgeInsets.all(marginSize),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Header(
+                      text: "Resume",
                     ),
-                  ),
-                  ResumeSection(
-                    title: "M. Sc. Informatics",
-                    date: "2020-2023",
-                    location: "Munich",
-                    institution: "Technical University of Munich",
-                    description:
-                        "Master studies in Computer Science with a focus on the area \"Machine Learning and Analytics\"",
-                  ),
-                  ResumeSection(
-                    title: "B. Sc. Computer Science",
-                    date: "2017-2020",
-                    location: "Stuttgart",
-                    institution: "DHBW Stuttgart",
-                    description:
-                        "Bachelor studies in Computer Science in cooperation with TRUMPF GmbH + Co. KG\n\nElective courses: Python, Data Science, Big Data",
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: marginSize),
-                    child: Text(
-                      "Work experience",
-                      style: h2Style,
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: marginSize),
+                      child: Text(
+                        "Education",
+                        style: h2Style,
+                      ),
                     ),
-                  ),
-                  ResumeSection(
-                    title: "Coorporate Student",
-                    date: "2017-2020",
-                    location: "Ditzingen",
-                    institution: "TRUMPF GmbH + Co. KG",
-                    description:
-                        "18 months of internships in various software development departments\n\nBachelor thesis: Developed a regression model for predicting manual labor times in sheet-metal production, evaluated various models from scikit-learn and XGBoost",
-                  ),
-                  ResumeSection(
-                    title: "Summer Intern",
-                    date: "05/2019-08/2019",
-                    location: "Farmington, CT, USA",
-                    institution: "TRUMPF Inc.",
-                    description:
-                        "4 month internship at TRUMPF North America within the software development department",
-                  ),
-                ],
+                    ResumeSection(
+                      title: "M. Sc. Informatics",
+                      date: "2020-2023",
+                      location: "Munich",
+                      institution: "Technical University of Munich",
+                      description:
+                          "Master studies in Computer Science with a focus on the area \"Machine Learning and Analytics\"",
+                    ),
+                    ResumeSection(
+                      title: "B. Sc. Computer Science",
+                      date: "2017-2020",
+                      location: "Stuttgart",
+                      institution: "DHBW Stuttgart",
+                      description:
+                          "Bachelor studies in Computer Science in cooperation with TRUMPF GmbH + Co. KG\n\nElective courses: Python, Data Science, Big Data",
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: marginSize),
+                      child: Text(
+                        "Work experience",
+                        style: h2Style,
+                      ),
+                    ),
+                    ResumeSection(
+                      title: "Coorporate Student",
+                      date: "2017-2020",
+                      location: "Ditzingen",
+                      institution: "TRUMPF GmbH + Co. KG",
+                      description:
+                          "18 months of internships in various software development departments\n\nBachelor thesis: Developed a regression model for predicting manual labor times in sheet-metal production, evaluated various models from scikit-learn and XGBoost",
+                    ),
+                    ResumeSection(
+                      title: "Summer Intern",
+                      date: "05/2019-08/2019",
+                      location: "Farmington, CT, USA",
+                      institution: "TRUMPF Inc.",
+                      description:
+                          "4 month internship at TRUMPF North America within the software development department",
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
